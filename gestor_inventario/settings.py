050139892f528f5783fe91c99b4e4f114d70edc0
David Palma <davidpalmalugo@gmail.com>
2019-04-22 23:55:41 -0400

    configuracion en settings.py

--- gestor_inventario/settings.py
+++ gestor_inventario/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gestor_inventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jdmclasic',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
