f77db91b149c9bd292ba6f2b6baf33e5a8716ce6
David Palma <davidpalmalugo@gmail.com>
2019-04-26 23:37:28 -0400

    Se configuraron las urls principales del proyecto

--- gestor_inventario/urls.py
+++ gestor_inventario/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inventario/', include('apps.inventario.urls')),
+    path('personal/', include('apps.personal.urls')),
+    path('registro/', include('apps.registro.urls')),
+    path('usuario/', include('apps.usuario.urls')),
+    path('', auth_views.LoginView.as_view(template_name="index.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="usuario/logout.html"), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
