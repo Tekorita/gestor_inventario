80afa1fc68255905ab3965eec3fd02d447cd43a4
David Palma <davidpalmalugo@gmail.com>
2019-04-28 00:08:21 -0400

    se creo el modelo de registros

--- apps/registro/models.py
+++ apps/registro/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from apps.inventario.models import Inventario
+from apps.personal.models import Personal
 # Create your models here.
+
+class Registro(models.model):
+    personal = models.ForeignKey(Personal, null=True, blank=True, on_delete = models.CASCADE)
+    insumo = models.ManyToManyField(Inventario, blank=True)
+    cantidad = models.IntegerField()
+    fecha = models.DateField()
+
+    
+
+
+
+
