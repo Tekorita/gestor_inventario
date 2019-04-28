929fef40b044446fdd231070ebdaf140c51e34fc
David Palma <davidpalmalugo@gmail.com>
2019-04-27 23:30:02 -0400

    se creo los atributos del modelo de inventario

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Inventario(models.Model):
+    descripcion = models.CharField(max_length=50)
+    cantidad = models.IntegerField()
+    fecha_ingreso = models.DateField()
