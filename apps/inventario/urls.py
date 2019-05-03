992d96d955291485bf98aa5bedd4498089f3a3ef
David Palma <davidpalmalugo@gmail.com>
2019-05-02 23:47:44 -0400

    se crearon las url para la vista de crear formulario

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from apps.inventario.views import InventarioList
+from apps.inventario.views import InventarioList, InventarioCreate
 
 urlpatterns = [
     path('listar', InventarioList.as_view(), name='inventario_listar'),
+    path('nuevo', InventarioCreate.as_view(), name='inventario_nuevo'),
 ]
