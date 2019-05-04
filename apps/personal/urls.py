67069227d51aa726d7e871f1103f35b274b36a0e
David Palma <davidpalmalugo@gmail.com>
2019-05-04 02:37:14 -0400

    configuracion en url para eliminar datos de personal

--- apps/personal/urls.py
+++ apps/personal/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
-from apps.personal.views import PersonalList, PersonalCreate, PersonalUpdate
+from apps.personal.views import PersonalList, PersonalCreate, PersonalUpdate, PersonalDelete
 
 urlpatterns = [
     path('listar', PersonalList.as_view(), name='personal_listar'),
     path('nuevo', PersonalCreate.as_view(), name='personal_nuevo'),
     path('editar/<pk>/', PersonalUpdate.as_view(), name ='personal_editar'),
+    path('eliminar/<pk>/', PersonalDelete.as_view(), name ='personal_eliminar'),
 ]
