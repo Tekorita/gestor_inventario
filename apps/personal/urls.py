011a544dc7df8b499eae6a8041b1e1b1511197ea
David Palma <davidpalmalugo@gmail.com>
2019-05-04 02:09:53 -0400

    Se configuro la urls para editar datos del personal

--- apps/personal/urls.py
+++ apps/personal/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from apps.personal.views import ListView, CreateView
+from apps.personal.views import ListView, CreateView, UpdateView
 
 urlpatterns = [
     path('listar', ListView.as_view(), name='personal_listar'),
     path('nuevo', CreateView.as_view(), name='personal_nuevo'),
+    path('editar' UpdateView.as_view(), name = 'personal_editar'),
 ]
