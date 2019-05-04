4fd1f2fd96ac7edb70289b3784d7455543789e18
David Palma <davidpalmalugo@gmail.com>
2019-05-03 23:54:36 -0400

    Se creo la vista para listar al personal

--- apps/personal/views.py
+++ apps/personal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from apps.personal.models import Personal 
+from django.urls import reverse_lazy, reverse
 
 # Create your views here.
+
+class PersonalList(ListView):
+    model = Personal
+    template_name = ''
+    
