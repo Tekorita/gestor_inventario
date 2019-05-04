4f6985837957690ff6d2709cdd12cf2cb4dbd0aa
David Palma <davidpalmalugo@gmail.com>
2019-05-04 01:59:21 -0400

    Se creo la vista para crear personal

--- apps/personal/views.py
+++ apps/personal/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from apps.personal.models import Personal 
 from django.urls import reverse_lazy, reverse
+from apps.personal.forms import PersonalForm
 
 # Create your views here.
 
@@ -9,5 +10,9 @@ class PersonalList(ListView):
     model = Personal
     template_name = 'personal/listar.html'
 
-
+class PersonalCreate(CreateView):
+    model = Personal
+    form_class = PersonalForm
+    template_name = 'pesonal/nuevo.html'
+    success_url = reverse_lazy('personal_listar')
     
