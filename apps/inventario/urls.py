40d911a0d790717733c427d303d9f18acf8f1ad0
David Palma <davidpalmalugo@gmail.com>
2019-05-03 23:48:21 -0400

    Se crearon las url para modificar inventario

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from apps.inventario.views import InventarioList, InventarioCreate
+from apps.inventario.views import InventarioList, InventarioCreate, InventarioUpdate, InventarioDelete
 
 urlpatterns = [
     path('listar', InventarioList.as_view(), name='inventario_listar'),
     path('nuevo', InventarioCreate.as_view(), name='inventario_nuevo'),
+    path('editar', InventarioUpdate.as_view(), name='inventario_editar'),
 ]
