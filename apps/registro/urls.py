5cecff1377047f305ca82d9436df87f0a787d831
David Palma <davidpalmalugo@gmail.com>
2019-05-05 23:52:38 -0400

    se creo las urls de las vistas de registro

---
+++ apps/registro/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+#from apps.personal.views import PersonalList, InventarioCreate, InventarioUpdate, InventarioDelete
+
+urlpatterns = [
+    #path('listar', InventarioList.as_view(), name='inventario_listar'),
+    #path('nuevo', InventarioCreate.as_view(), name='inventario_nuevo'),
+    #path('editar/<pk>/', InventarioUpdate.as_view(), name='inventario_editar'),
+    #path('eliminar/<pk>/', InventarioDelete.as_view(), name='inventario_eliminar'),
+]
