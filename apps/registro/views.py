a9fbdfb6f43277d57e717cf3b9025637658778eb
David Palma <davidpalmalugo@gmail.com>
2019-05-05 23:55:45 -0400

    se desarrollo la vista para listar registros

--- apps/registro/views.py
+++ apps/registro/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from apps.registro.models import Registro
 # Create your views here.
+
+class RegistroList(ListView):
+    model = Registro
+    template_name = 'registro/listar.html'
