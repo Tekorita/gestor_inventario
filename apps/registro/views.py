fd6ff56bb2fc0dbf53cd48b952b316c2f3e4e047
David Palma <davidpalmalugo@gmail.com>
2019-05-10 23:42:43 -0400

    correccion de errores en vista

--- apps/registro/views.py
+++ apps/registro/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from apps.registro.models import Registro
 # Create your views here.
 
