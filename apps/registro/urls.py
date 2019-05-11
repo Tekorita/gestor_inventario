5397d6c140507b546e28950512810d234d550fbf
David Palma <davidpalmalugo@gmail.com>
2019-05-10 23:47:12 -0400

    configuracion de urls para vista de registro

--- apps/registro/urls.py
+++ apps/registro/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-#from apps.personal.views import PersonalList, InventarioCreate, InventarioUpdate, InventarioDelete
+from apps.registro.views import RegistroList, RegistroCreate, RegistroUpdate, RegistroDelete
 
 urlpatterns = [
-    #path('listar', InventarioList.as_view(), name='inventario_listar'),
-    #path('nuevo', InventarioCreate.as_view(), name='inventario_nuevo'),
-    #path('editar/<pk>/', InventarioUpdate.as_view(), name='inventario_editar'),
-    #path('eliminar/<pk>/', InventarioDelete.as_view(), name='inventario_eliminar'),
+    path('listar', RegistroList.as_view(), name='registro_listar'),
+    path('nuevo', RegistroCreate.as_view(), name='registro_nuevo'),
+    path('editar/<pk>/', RegistroUpdate.as_view(), name='registro_editar'),
+    path('eliminar/<pk>/', RegistroDelete.as_view(), name='registro_eliminar'),
 ]
