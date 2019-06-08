5eb7fa5469a2ccb77acb1372b20f12e369879c65
David Palma <davidpalmalugo@gmail.com>
2019-06-07 22:31:24 -0400

    nueva clase en modelo de personal

--- apps/personal/models.py
+++ apps/personal/models.py
@@ -9,3 +9,11 @@ class Personal(models.Model):
 
     def __str__(self): #esta funcion nos trae el valor del objeto en vez del nombre del objeto cuando es llave foranea o forekeing
         return '{} {}'.format(self.nombre, self.apellido)
+
+class Personal2(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    cargo = models.CharField(max_length=50)
+
+    def __str__(self): #esta funcion nos trae el valor del objeto en vez del nombre del objeto cuando es llave foranea o forekeing
+        return '{} {}'.format(self.nombre, self.apellido)
