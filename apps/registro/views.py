78a42246626cfffeb9fb1147cc5956c3ad3172dc
David Palma <davidpalmalugo@gmail.com>
2019-06-11 23:46:00 -0400

    nuevas clases en el view de registro para agregar y editar

--- apps/registro/views.py
+++ apps/registro/views.py
@@ -23,3 +23,17 @@ class RegistroDelete(DeleteView):
     model = Personal
     template_name = 'personal/eliminar.html'
     success_url = reverse_lazy('personal_listar')
+
+from apps.registro.models import Registro
+# Create your views here.
+
+class RegistroList(ListView):
+    model = Registro
+    template_name = 'registro/listar.html'
+
+class RegistroCreate(CreateView):
+    model = Registro
+    form_class = RegistroForm
+    template_name = 'registro/nuevo.html'
+    success_url = reverse_lazy('registro_listar')
+
