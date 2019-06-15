b789c44885d66890b0de67e98e7b6fb736140fd1
David Palma <davidpalmalugo@gmail.com>
2019-06-14 23:57:49 -0400

    correcciones en view de registros

--- apps/registro/views.py
+++ apps/registro/views.py
@@ -24,16 +24,3 @@ class RegistroDelete(DeleteView):
     template_name = 'personal/eliminar.html'
     success_url = reverse_lazy('personal_listar')
 
-from apps.registro.models import Registro
-# Create your views here.
-
-class RegistroList(ListView):
-    model = Registro
-    template_name = 'registro/listar.html'
-
-class RegistroCreate(CreateView):
-    model = Registro
-    form_class = RegistroForm
-    template_name = 'registro/nuevo.html'
-    success_url = reverse_lazy('registro_listar')
-
